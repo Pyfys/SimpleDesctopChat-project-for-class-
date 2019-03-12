6abbbab021cb3282d4710a3fec4fa126f3bf3a9e
Pyfys <45160930+Pyfys@users.noreply.github.com>
2019-03-12 12:09:43 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+/**
+ *
+ *  @author Shkambara Dmytro S15163
+ *
+ */
+
+package zad1;
+
+import javax.swing.JFrame;
+
+public class Main{
+	
+public static void main(String[] args) throws Exception {
+    Service s = new Service("Poland");
+    String weatherJson = s.getWeather("Warsaw");
+    Double rate1 = s.getRateFor("USD");
+    Double rate2 = s.getNBPRate();
+    
+    GUI gui = new GUI();
+    
+    JFrame frame = gui.loadGUI(weatherJson, s.getCity(),s.getTemp(),s.getWeath(),s.getCountryCurency(),s.getCurency(),rate1,rate2);
+    frame.show();
+    
+    
+	}
+}
